3007a42823ebf1c082813494b781114de2733ea7
JuliaGrisales <jgrisales@gmail.com>
2020-06-30 12:50:16 +0200

    Optimizing

--- config/yargs.js
+++ config/yargs.js
@@ -1,30 +1,24 @@
+const description = {
+  demand: true,
+  alias: "d",
+  description: "Description of the task to do"
+}
+
+const completed = {
+  default: true,
+  alias: "c",
+  description: "Mark as completed or pending the task"
+}
+
 const argv = require("yargs")
   .command("create", "Create a task to do", {
-    description: {
-      demand: true,
-      alias: "d",
-      description: "Description of the task to do"
-    }
+    description
   })
   .command("update", "Update the satate o f task", {
-    description: {
-      demand: true,
-      alias: "d",
-      description: "Description of the task to do"
-    },
-    completed: {
-      default: true,
-      alias: "c",
-      description: "Mark as completed or pending the task"
-    }
-  })
+    description, 
+    completed})
   .command("remove", "Remove a thast the satate o f task", {
-    description: {
-      demand: true,
-      alias: "d",
-      description: "Description of the task to do"
-    }
-  })
+    description})
   .help().argv
 
 module.exports = {
